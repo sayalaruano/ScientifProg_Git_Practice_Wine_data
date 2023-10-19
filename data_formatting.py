10530be7fca8fd070a51142340a0216742fb88c8
CheYeeNg <ngcheyee@gmail.com>
2023-10-19 11:39:21 +0200

    replaced function var with general descriptions

--- data_formatting.py
+++ data_formatting.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 def data_formatting(data):
-    wine_data = pd.read_excel(data, header = None)
+    data = pd.read_excel(data, header = None)
     class_1 = [1] * 59
     class_2 = [2] * 71
     class_3 = [3] * 48
     class_vector = class_1 + class_2 + class_3
 
-    wine_data_with_class = wine_data.assign(Classes = class_vector)
-    return wine_data_with_class
+    data_with_class = data.assign(Classes = class_vector)
+    return data_with_class
 
 wine_data = data_formatting("Wine.xlsx")
 #print(wine_data)
