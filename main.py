9c0347a94f2d41f1bd4549f3ace6337430fab7f6
CheYeeNg <ngcheyee@gmail.com>
2023-10-19 12:45:50 +0200

    put functions together - draft version

---
+++ main.py
@@ -0,0 +1,27 @@
+# If needed, before starting, change the current working directory by uncommenting and inserting the right path in the following line of code.
+import os
+os.chdir("/home/coder/projects/Scientific Programming")
+
+from data_formatting import *
+from Scaling import *
+from explore_PCA import *
+from VisualisationTool import *
+
+# Read in the wine dataset and append the classes
+wine_data = data_formatting("Wine.xlsx")
+
+# Do different kind of scaling
+mean_cent_scal_wine_data = mean_cent_scal_df = mean_centering_scale(wine_data)
+autoscaling_wine_data = autoscaling_df = autoscaling(wine_data)
+pareto_scal_wine_data = pareto_scal_df = pareto_scaling(wine_data)
+
+# Using the autoscaling data for PCA
+pca = pca_plot(autoscaling_wine_data)
+
+# Visualize PCA
+tool = VisualisationTool()
+tool.plot_pca_simple(pca, ...)
+
+
+
+
